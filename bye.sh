ebc4909d964101e1536ef22852f3a9a3008b14f0
Marko Shiva Pavlovic - in1t3r <marko.shiva.pavlovic@gmail.com>
2017-07-17 23:54:03 +0200

    stupid shell script

---
+++ bye.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#Warning this script contains profanity used just for fun.
+
+action=$1
+name=$2
+if [[ $# -le 1 ]]
+then
+	echo "###################################################################"
+	echo "run with single arguments, bye, hate, shitstorm"
+	exit 1
+fi
+if [[ $# == 2 ]]
+then
+	case $action in
+	bye)
+		echo "Goodbye ${name}"
+		;;
+	hate)
+		echo "Go away ${name}, I hate you."
+		;;
+	shitstorm)
+		echo "You stupid fuck ${name} I'm going to kill you fry you and serve to your children"
+		;;
+esac
+fi
