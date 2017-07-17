7936dd43438b2e5ce80582cae019a61f1af2931f
Marko Shiva Pavlovic - in1t3r <marko.shiva.pavlovic@gmail.com>
2017-07-17 22:08:27 +0200

    I will replace you with very small program

---
+++ byebitches/bye.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void bye(const char* person);
+void bye(const char* person) {
+  printf("Goodbye %s\n", person );
+}
+
+int main(int argc, char const *argv[]) {
+
+  bye(argv[1]);
+  return 0;
+}
